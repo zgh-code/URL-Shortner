343b735a4a022c55686e221fc8532506ee41b7ac
zgh-code <293354735+zgh-code@users.noreply.github.com>
2026-06-18 21:05:19 +1000

    added prisma client and adapter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+//client turns the ts into queries
+import { PrismaClient } from './generated/prisma/client'
+
+//and the pg just talks to postgre
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 
 //load the dot env
 dotenv.config()
