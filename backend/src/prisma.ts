89c723faf3b0b68280d8934dc6a0953e3fa92de5
zgh-code <293354735+zgh-code@users.noreply.github.com>
2026-06-23 19:31:59 +1000

    hotfix for prisma.ts to call the .env file

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from './generated/prisma/client'
 
 //and the pg just talks to postgre
 import { PrismaPg } from '@prisma/adapter-pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
